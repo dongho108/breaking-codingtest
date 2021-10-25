19bd27ba4e0ed5d1f2c57bd66b45758ac79852f6
dongho1 <dongho108@naver.com>
2021-10-25 11:10:01 +0900

    update dh_b15649.py (#9)

--- week3/mon/dh_b15649.py
+++ week3/mon/dh_b15649.py
@@ -1,11 +1,33 @@
-from itertools import permutations
+# from itertools import permutations
+#
+# n, m = map(int, input().split())
+#
+# num = [i + 1 for i in range(n)]
+# pers = list(permutations(num, m))
+#
+# for per in pers:
+#     for i in range(m):
+#         print(per[i], end=' ')
+#     print()
 
 n, m = map(int, input().split())
 
 num = [i + 1 for i in range(n)]
-pers = list(permutations(num, m))
+visited = [False for _ in range(n + 1)]
+results = []
 
-for per in pers:
-    for i in range(m):
-        print(per[i], end=' ')
-    print()
+
+def backtracking(depth):
+    if depth == m:
+        print(' '.join(map(str, results)))
+        return
+    for i in range(1, n + 1):
+        if not visited[i]:
+            visited[i] = True
+            results.append(i)
+            backtracking(depth + 1)
+            visited[i] = False
+            results.pop()
+
+
+backtracking(0)

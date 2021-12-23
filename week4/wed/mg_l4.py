6a1af598f88d92a51faf067087ae34d36dc1a663
Migon <79758079+konrini@users.noreply.github.com>
2021-12-23 10:12:03 +0900

    add mg_l4.py (#16)

---
+++ week4/wed/mg_l4.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums1.extend(nums2)
+        nums1.sort()
+        if len(nums1) % 2 == 0:
+            return (nums1[int(len(nums1)/2-1)] + nums1[int(len(nums1)/2)])/2
+        return nums1[int(len(nums1)/2)]
